823202c73e51d29ebbb1435175f109a790fa0856
Mario <mario-tubay@hotmail.com>
2021-01-20 22:09:48 -0500

    ArrayList

--- src/deber1/Deber1.java
+++ src/deber1/Deber1.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package deber1;
 
-/**
- *
- * @author mario
- */
-public class Deber1 {
+import Class.Persona;
+import java.util.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Deber1 {
+    static ArrayList<Persona> listPersona = new ArrayList<Persona>();
+    public static void main(String[] args) {    
+    
+        
+       
+        
+        Scanner sc = new Scanner(System.in);
+        int edad = 0;
+        String nombre;
+        String apellido;
+        String cedula;
+        boolean bucle= true;
+        do {        
+            System.out.println("Ingrese la edad para continuar: ");
+            edad = sc.nextInt();
+            if(edad>=18){
+                System.out.println("Ingrese el nombre: ");
+                nombre= sc.next();
+                System.out.println("Ingrese el Apellido: ");
+                apellido = sc.next();
+                System.out.println("Ingrese la cedula: ");
+                cedula = sc.next();
+                bucle = true;
+                Persona persona = new Persona(cedula, nombre, apellido, edad);
+               
+                listPersona.add(persona);
+                System.out.println("Datos ingresados con exito");
+                for (int i =0; i<listPersona.size(); i++){
+                    System.out.println(""+listPersona.get(i).toString());
+                }
+            }else{
+                System.out.println("Lo sentimos es menor de edad");
+                bucle = false;
+            }
+        } while (bucle);
+        
+       
+    
     }
+    
     
 }
